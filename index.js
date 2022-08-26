07611afcf2387be632f4479b1e540ba855a4cbd3
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-08-26 21:50:01 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+const body=document.querySelector("body");
+  const submitData =(username,email)=>{
+    const data={
+    name : "Steve",
+    email : "steve@steve.com",
+  };
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  fetch("http://localhost:3000/users", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (json) {
+    body.append(json.id)
+  })
+  .catch(function (error) {
+    alert('Unauthorized Access')
+    body.append(error)
+  })
+
+}
